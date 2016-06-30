6480c022f9df984d9bcacb10f7c3e68efb461623
Matthew Giem and Molly McGlone <matthew@>
2016-06-30 12:01:01 -0700

    slightly functional code

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,5 +2,16 @@ require('pry')
 
 class String
   define_method(:anagram)  do
+    numbers=*(1..36)
+    letter_array = self.gsub(/\s+/, /[[:punct:]]/, "").downcase.split("")
+    # letter_array = self.delete(" ").downcase.split("")
+    letter_array.each do |letter|
+      numbers[letter.to_i(36)]+=1
+    end
+    letter_array.join
   end
 end
+
+
+
+# gsub(/[[:punct:]]/, '')
